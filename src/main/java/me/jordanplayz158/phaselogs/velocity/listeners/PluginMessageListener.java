f546a0e1f78819e89c3567c5e20afe33b9b4a450
Jordan Adams <jordan@jordanplayz158.me>
2021-04-29 16:01:53 -0400

    PluginMessageListener.java - Removed unused method

--- src/main/java/me/jordanplayz158/phaselogs/velocity/listeners/PluginMessageListener.java
+++ src/main/java/me/jordanplayz158/phaselogs/velocity/listeners/PluginMessageListener.java
@@ -1,14 +1,11 @@
 package me.jordanplayz158.phaselogs.velocity.listeners;
 
-import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.PluginMessageEvent;
 import com.velocitypowered.api.proxy.Player;
-import com.velocitypowered.api.proxy.messages.MinecraftChannelIdentifier;
 import me.jordanplayz158.phaselogs.velocity.PhaseLogs;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,26 +16,23 @@ public class PluginMessageListener {
             return;
         }
 
-        /* args[0] = uuid
-         * args[1] = prefix
-         * args[2] = user (with ":")
-         * args[3] = message
+        /* args[0] = prefix
+         * args[1] = user (with ":")
+         * args[2] = message
          */
         String[] args = ByteStreams.newDataInput(event.getData()).readUTF().split("\\s+");
-        Player player = PhaseLogs.getInstance().getServer().getPlayer(args[2].substring(0, args[2].length() - 1)).orElse(null);
-
-        sendCustomData(player, args[0]);
+        Player player = PhaseLogs.getInstance().getServer().getPlayer(args[1].substring(0, args[1].length() - 1)).orElse(null);
 
         StringBuilder format = new StringBuilder(PhaseLogs.getInstance().getConfig().get("botOutput"));
         StringBuilder message = new StringBuilder();
 
-        for(int i = 3; i < args.length; i++) {
+        for(int i = 2; i < args.length; i++) {
             message.append(args[i]).append(" ");
         }
 
         Map<String, String> placeholders = new HashMap<String, String>() {{
             put("{SERVER}", player.getCurrentServer().get().getServerInfo().getName());
-            put("{PREFIX}", args[1]);
+            put("{PREFIX}", args[0]);
             put("{USERNAME}", player.getUsername());
             put("{MESSAGE}", message.toString());
         }};
@@ -70,20 +64,5 @@ public class PluginMessageListener {
         }
 
         PhaseLogs.getInstance().addMessage(format.toString());
-    }
-
-    public void sendCustomData(Player player, String id) {
-        Collection<Player> networkPlayers = PhaseLogs.getInstance().getServer().getAllPlayers();
-        // perform a check to see if globally are no players
-        if (networkPlayers == null || networkPlayers.isEmpty()) {
-            return;
-        }
-        ByteArrayDataOutput out = ByteStreams.newDataOutput();
-        out.writeUTF(id); // this data could be whatever you want
-
-        // we send the data to the server
-        // using ServerInfo the packet is being queued if there are no players in the server
-        // using only the server to send data the packet will be lost if no players are in it
-        player.sendPluginMessage(MinecraftChannelIdentifier.create("logmessage","received"), out.toByteArray());
     }
 }
